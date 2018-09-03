ec76a37086abb8c16f58468d368862b90920a776
cezar <cezarcbl@gmail.com>
2018-09-03 11:07:36 -0300

    add tests

--- robot/robot_utils.py
+++ robot/robot_utils.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)) + '/src')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/src')
 from simulation import Simulation
 import time
 from scipy.io import loadmat, savemat
